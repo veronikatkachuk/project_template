93018cc5846b8e26ebd085978c02c729e06a6778
Veronika Tkachuk <veronikatkachuk11@gmail.com>
2025-04-25 17:56:27 +0300

    add input.py and output.py

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def read_from_console():
+    pass
+
+
+def read_from_file_builtin(filepath):
+    pass
+
+
+def read_from_file_pandas(filepath):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def print_to_console(text):
+    pass
+
+
+def write_to_file_builtin(filepath, text):
+    pass
